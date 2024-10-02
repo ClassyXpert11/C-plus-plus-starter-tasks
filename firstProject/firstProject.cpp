9591fa43afec395f309c60f6670e9ec9648d9230
Abdullah Amod <144704432+ClassyXpert11@users.noreply.github.com>
2024-10-02 12:20:45 +0100

    Created my first project

---
+++ firstProject/firstProject.cpp
@@ -0,0 +1,47 @@
+// firstProject.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    // Task a - Name, course, student id
+    std::cout << "--------\n";
+    std::cout << "Name: Abdullah\n";
+    std::cout << "Course: Games Desgin and Programming\n";
+    std::cout << "Student ID: a013971o\n";
+    std::cout << "--------\n\n";
+
+    // Task b - ascii art right angled trianlge
+    std::cout << "*\n";
+    std::cout << "**\n";
+    std::cout << "*  *\n";
+    std::cout << "****\n\n";
+
+    // Task c - making a little boat
+    std::cout << "    *\n";
+    std::cout << "    **\n";
+    std::cout << "    *  *\n";
+    std::cout << "    *****\n";
+    std::cout << "***********\n";
+    std::cout << "*         *\n";
+    std::cout << " *********\n";
+
+
+
+    // Task d
+    std::cout << "Hello... \t\t\t\t there!";
+
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
