9cca2cd98de6dee228b6af0ca1dcda44c4ef445b
Abdullah <144704432+ClassyXpert11@users.noreply.github.com>
2024-10-03 10:44:31 +0100

    did some of the tutorial 2 stuff

---
+++ c++ variables and if statements/c++ variables and if statements/c++ variables and if statements.cpp
@@ -0,0 +1,22 @@
+// c++ variables and if statements.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << 5 + 5 << "\n";
+
+    cout << 355 /113 << "\n";
+
+    cout << 503 * 92 << "\n";
+
+    int num1 = 0;
+    while (num1 < 10) {
+        num1 += 1;
+        cout << num1 << "\n";
+    }
+
+}
+
