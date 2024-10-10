0583c98e5e0d6f7da11b574764bcdd00fcbf8f5e
Batman <144704432+ClassyXpert11@users.noreply.github.com>
2024-10-10 10:43:40 +0100

    did some more tasks

--- c++ variables and if statements/c++ variables and if statements/c++ variables and if statements.cpp
+++ c++ variables and if statements/c++ variables and if statements/c++ variables and if statements.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int main()
 {
-    cout << 5 + 5 << "\n";
+    cout << 5 + 5 << "\n\n";
 
-    cout << 355 /113 << "\n";
+    cout << 355 /113 << "\n\n";
 
-    cout << 503 * 92 << "\n";
+    cout << 503 * 92 << "\n\n";
 
     int num1 = 0;
     while (num1 < 10) {
@@ -15,8 +15,31 @@ int main()
         cout << num1 << "\n";
     }
 
-    five_factorial = 5 * 4 * 3 * 2 * 1;
-    cout << five_factorial;
+    int five_factorial = 5 * 4 * 3 * 2 * 1;
+    cout << "\n" << five_factorial << "\n\n";
+
+    void six_times_table(); {
+        int num1 = 1;
+        int num2 = 6;
+
+        while (num1 <= 10) {
+            int num3 = num1 * num2;
+            cout << num2 << " x " << num1 << " = " << num3 << "\n";
+            num1 += 1;
+        }
+    }
+
+    int three_e = ((919 * -1) - 6) / 2.56;
+    cout <<"\n" << three_e << "\n\n";
+
+    int mean = (15 + 25 + 30 + 20) / 4;
+    cout << mean << "\n\n";
+
+    int fraction_one = 0.50;
+    int fraction_two = 0.75;
+    int result_fraction = fraction_one+fraction_two;
+
+    cout << result_fraction << "\n\n";
 
 }
 
