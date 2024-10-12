105c1abbc0ce1d7e377449c07861b3e2cb2fcecc
Abdullah <144704432+ClassyXpert11@users.noreply.github.com>
2024-10-12 16:35:19 +0100

    Update c++ variables and if statements.cpp

--- c++ variables and if statements/c++ variables and if statements/c++ variables and if statements.cpp
+++ c++ variables and if statements/c++ variables and if statements/c++ variables and if statements.cpp
@@ -3,21 +3,28 @@ using namespace std;
 
 int main()
 {
+    // using c++ as a artithmati calculator
+    cout << "1)\n";
     cout << 5 + 5 << "\n\n";
 
+    cout << "2)\n";
     cout << 355 /113 << "\n\n";
 
+    cout << "3a)\n";
     cout << 503 * 92 << "\n\n";
 
+    cout << "3b)\n";
     int num1 = 0;
     while (num1 < 10) {
         num1 += 1;
         cout << num1 << "\n";
     }
 
+    cout << "\n3c)\n";
     int five_factorial = 5 * 4 * 3 * 2 * 1;
-    cout << "\n" << five_factorial << "\n\n";
+    cout << five_factorial << "\n\n";
 
+    cout << "3d)\n";
     void six_times_table(); {
         int num1 = 1;
         int num2 = 6;
@@ -29,19 +36,27 @@ int main()
         }
     }
 
+    cout << "\n3e)\n";
     int three_e = ((919 * -1) - 6) / 2.56;
-    cout <<"\n" << three_e << "\n\n";
+    cout << three_e << "\n\n";
 
+    cout << "3f)\n";
     int mean = (15 + 25 + 30 + 20) / 4;
     cout << mean << "\n\n";
 
-    int fraction_one = 0.50;
-    int fraction_two = 0.75;
-    int result_fraction = fraction_one+fraction_two;
+    cout << "3g)\n";
+    int a = 1;
+    int b = 2;
+    int c = 3;
+    int d = 4;
 
-    cout << result_fraction << "\n\n";
+    int numerator = a + c;
+    int denominator = b + d;
 
-    // this is a comment
+    cout << numerator << "/" << denominator << endl;
+    
+
+    
 
 }
 
